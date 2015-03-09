5e20cc1321d9f137a1b7ebcf8348b37279ccd289
Alyssa Kornylo <alyssako@umich.edu>
2015-03-09 19:02:22 -0400

    implemented task 4 in image processor

---
+++ src/a2/MagicNumbers.hpp
@@ -0,0 +1,11 @@
+#ifndef MAGIC_NUMBERS_HPP
+#define MAGIC_NUMBERS_HPP
+
+#define wx1 11
+#define wy1 11
+#define wx2 11
+#define wy2 -11
+#define wx3 -11
+#define wy3 11
+
+#endif
--- src/a2/ip.c
+++ src/a2/ip.c
@@ -8,11 +8,18 @@ struct state {
     parameter_gui_t *pg;
     
     //A2 stuff
-    int mode; //which mode of operation are we currently in
+    int mode; // which mode of operation are we currently in
+              // mode = 1: mask mode
+              // mode = 2: color picker mode
+              // mode = 3: calibration mode
     bool capture; //has image been captured from camera
     image_u32_t *u32_im;
     image_u32_t *revert;
     int fsm_counter;
+    
+    double x1, x2, x3, y1, y2, y3;
+    int cal_counter;
+
     double Hmin, Hmax, Smin, Smax, Vmin, Vmax;
     HSV_p cp_array[5];
     int cp_index;
@@ -200,18 +207,22 @@ key_event (vx_event_handler_t *vxeh, vx_layer_t *vl, vx_key_event_t *key)
                 (void) image_u32_write_pnm(state->u32_im, "cam_image.pnm");
 
                 //RMC - fix this for user selected image saves
-                //printf("enter name for image:\n");
-                //char path[100];
-                //char *ret = fgets(path, 100, stdin);
+                printf("enter name for image:\n");
+                char path[100];
+                char *ret = fgets(path, 100, stdin);
                 
-                //printf("path is : %s\n", path);
-                //if (ret == path)
-                //{
-                    //char path[1024];
-                    //strcat(path, in_buf);
-                    //strcat(path, "pnm");
-                    //(void) image_u32_write_pnm(state->u32_im, path);
-                //}
+                printf("path is : %s\n", path);
+                if (ret == path)
+                {
+                    char path[1024];
+
+                    // replace \n with null character because fgets is terrible
+                    int len = strlen(path);
+                    path[len - 2] = '\0';
+                    
+                    strcat(path, "pnm");
+                    (void) image_u32_write_pnm(state->u32_im, path);
+                }
             }
         }
     }
@@ -337,6 +348,8 @@ state_create (void)
     state->Vmin = -1.0;
     state->Vmax = -1.0;
     state->cp_index = 0;
+
+    state->cal_count = 0;
 
     state->running = 1;
 
